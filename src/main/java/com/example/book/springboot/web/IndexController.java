a9bfc5351c4edab9ec550e87b6db8602a4f31f04
danny <gryb809@gmail.com>
2021-03-04 21:46:05 +0900

    :sparkles: 게시물 조회 기능 추가 및 메인 화면 수정

--- src/main/java/com/example/book/springboot/web/IndexController.java
+++ src/main/java/com/example/book/springboot/web/IndexController.java
@@ -1,13 +1,20 @@
 package com.example.book.springboot.web;
 
+import com.example.book.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("post", postsService.findAllDesc());
         return "index";
     }
 
