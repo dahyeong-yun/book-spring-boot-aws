56b42509fc5877998f202bef5a977b7eee99fa6a
danny <gryb809@gmail.com>
2021-03-29 07:46:58 +0900

    :construction: 배포환경 profile 확인을 위한 커밋

--- src/main/java/com/example/book/springboot/web/ProfileController.java
+++ src/main/java/com/example/book/springboot/web/ProfileController.java
@@ -17,8 +17,8 @@ public class ProfileController {
     @GetMapping("/profile")
     public String profile() {
         List<String> profiles = Arrays.asList(env.getActiveProfiles());
-        List<String> realProfiles = Arrays.asList("real", "real1,", "real2");
-        String defaultProfile = profiles.isEmpty() ? "default" : profiles.get(0);
+        List<String> realProfiles = Arrays.asList("real", "real1", "real2");
+        String defaultProfile = profiles.isEmpty() ? "default" : profiles.toString();
         return profiles.stream()
                 .filter(realProfiles::contains)
                 .findAny()
