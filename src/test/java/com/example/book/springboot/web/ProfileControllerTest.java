f4dfa58a0ca04a37f87738bbd48032aa311a5adb
danny <gryb809@gmail.com>
2021-03-28 19:23:29 +0900

    :pencil2: health.sh 괄호 수정

--- src/test/java/com/example/book/springboot/web/ProfileControllerTest.java
+++ src/test/java/com/example/book/springboot/web/ProfileControllerTest.java
@@ -2,14 +2,12 @@ package com.example.book.springboot.web;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mock.env.MockEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
